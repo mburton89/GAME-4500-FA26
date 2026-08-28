b26e02bb5f84621a0096a9b04b17db275909e065
johnathanimboden <johnathanimboden90@gmail.com>
2026-08-28 17:00:46 -0500

    Main map

---
+++ FPSUnity/Assets/Scripts/Canon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    public float fireRate;
+    public float shootVelocity;
+    public GameObject projectileprefab;
+    public AudioSource shootSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+
+    }
+}
