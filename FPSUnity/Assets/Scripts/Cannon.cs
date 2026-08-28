02647151ece1a639a978e4b539ddc62c62c835e9
shadicl <itshard532@gmail.com>
2026-08-28 16:56:07 -0500

    Whiteboxing
    a

---
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float fireRate;
+    public float shootVelocity;
+    public GameObject projectilePrefab;
+    public AudioSource shootSound;
+
+    void Shoot()
+    {
+
+    }
+}
