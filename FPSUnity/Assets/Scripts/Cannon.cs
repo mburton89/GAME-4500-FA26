ed35e4416b86a34d2d6d848fc09badd72047bdef
malaynabock <malaynabock@webster.edu>
2026-08-28 16:56:39 -0500

    malaynaBock-aug28-session1

---
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float fireRate;
+    public float shootVelocity;
+    public GameObject projectilePrefab;
+    public AudioSource shootSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        //how shoot functions
+    }
+
+}
