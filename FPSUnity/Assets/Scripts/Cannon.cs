7fff5d7b433e84dcf81a8df1e5f2e14c234d1539
WUStudent <wustudent@webster.edu>
2026-08-28 16:54:58 -0500

    Made Level And Shoot Script

---
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+    public float firerate;
+    public float shootVelocity;
+    public GameObject projectilePrefab;
+    public AudioSource shootSound;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void Shoot()
+        {
+
+        }
+
+    }
+}
