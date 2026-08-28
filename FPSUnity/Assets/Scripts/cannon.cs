9e35e27468c41444ab37d728f371d4a6e57eae13
WUStudent <ejljhl0582@gmail.com>
2026-08-28 16:56:15 -0500

    Whiteboxing complete

---
+++ FPSUnity/Assets/Scripts/cannon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class cannon : MonoBehaviour
+{
+    public float fireRate;
+    public float shootVelocity;
+    public GameObject projectilePreFab;
+    public AudioSource shootSound;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void shoot()
+    {
+        //our shoot code
+    }
+}
