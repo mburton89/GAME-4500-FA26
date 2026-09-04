591dd158f50f3b5b0658062671c89fafd4a34add
WUStudent <wustudent@webster.edu>
2026-09-04 16:25:05 -0500

    Zom/Projectile/Ai

--- FPSUnity/Assets/Scripts/Cannon.cs
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -6,6 +6,7 @@ public class Cannon : MonoBehaviour
     public float shootVelocity;
     public GameObject projectilePrefab;
     public AudioSource shootSound;
+    public Transform projectileSpawnPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,12 +16,28 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     void Shoot()
     {
+        //OUT SHOOT CODE
+        print("Shoot");
 
+        //Step 1: Instantiate/Spawn projectile prefab in projectile spawn point
+        GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+        //Step 2: Add forward force to it
+        newProjectile.GetComponent<Rigidbody>().AddForce(projectileSpawnPoint.forward * shootVelocity);
+
+        //Step 3: Make plunk SFX
+        shootSound.Play();
+
+        //Step 4: Despawn projectile after X seconds
+        Destroy(newProjectile, 5);
     }
 
 }
