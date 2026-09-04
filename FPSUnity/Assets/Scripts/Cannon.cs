19ddc2108ea6aaf4e12935f9fafe193801b17309
johnathanimboden <johnathanimboden90@gmail.com>
2026-09-04 16:26:48 -0500

    Made player shoot and made zombie
    added shoot function to player and added zombie that will chase the player

--- FPSUnity/Assets/Scripts/Cannon.cs
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -7,6 +7,7 @@ public class Cannon : MonoBehaviour
     public float shootVelocity;
     public GameObject projectilePrefab;
     public AudioSource shootSound;
+    public Transform projectileSpawnPoint;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,12 +19,28 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     void Shoot()
     {
         // OUR SHOOT CODE
+        print("shoot");
+
+        //step 1: instantiate/spawn projectile prefab in projectile spawn point
+        GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+
+        //step 2: add forward force to it
+        newProjectile.GetComponent<Rigidbody>().AddForce(projectileSpawnPoint.forward * shootVelocity);
+        
+        //step 3: make plunk SFX
+        shootSound.Play();
+
+        //step 4: Despawn projectile after x seconds
+        Destroy(newProjectile, 5);
     }
 
 }
---
+++ FPSUnity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damageToGive;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Zombie>())
+            {
+            //We've collided with a Zombie
+            collision.gameObject.GetComponent<Zombie>().Takedamage(damageToGive);
+
+            }
+    }
+
+}
