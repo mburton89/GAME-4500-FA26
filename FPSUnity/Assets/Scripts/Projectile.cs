771a326beae71ac66216b338a51db871ca2926f2
Kaleb Means <kalebmeans@gmail.com>
2026-09-04 16:25:04 -0500

    zombie and shoot

---
+++ FPSUnity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float damageToGive;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Zombie>())
+        {
+            //we've collided with a zombie
+            collision.gameObject.GetComponent<Zombie>().TakeDamage(damageToGive);
+        }
+    }
+}
---
+++ FPSUnity/Assets/Scripts/Zombie.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Zombie : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float maxHealth = 5;
+    float currentHealth;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    public GameObject zombieGuts;
+
+    public AudioSource takeDamageSound;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+        target = FindObjectOfType<FPSController>().transform;
+
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.speed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    void ChasePlayer()
+    {
+        agent.destination = target.position;
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        currentHealth -= damageToTake;
+
+        if(currentHealth <= 0)
+        {
+            Instantiate(zombieGuts, transform.position, transform.rotation, null);
+            Destroy(gameObject);
+        }
+    }
+
+}
