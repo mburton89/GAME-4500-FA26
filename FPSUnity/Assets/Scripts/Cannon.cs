7af6b2dc3b058e35f360ab76128550b59495a24a
WUStudent <wustudent@webster.edu>
2026-09-04 16:25:33 -0500

    Zombie and Projectile
    Zombie can run, take damage from projectile, and we can kill zombie now

--- FPSUnity/Assets/Scripts/Cannon.cs
+++ FPSUnity/Assets/Scripts/Cannon.cs
@@ -6,6 +6,7 @@ public class Cannon : MonoBehaviour
     public float shootVelocity;
     public GameObject projectilePrefab;
     public AudioSource shootSound;
+    public Transform projectileSpawnPoint;
         
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,13 +18,33 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Shoot();
+
+        }
     }
 
     void Shoot()
     {
 
         //OUR SHOOT CODE//
+        print("Shoot");
+
+        //Step 1: Instant/Spawn projectile prefab
+        GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+
+        //Step 2: Add forward force to it
+        newProjectile.GetComponent<Rigidbody>().AddForce(projectileSpawnPoint.forward * shootVelocity
+            );
+
+        //Step 3: Make plunk SFX
+
+        shootSound.Play();
+
+        //Step 4: Despawn projectile after x seconds
+        Destroy( newProjectile, 5);
 
     }
 
