3b22ce7dcc83e3bf655b1b60426cd0f238888fe7
WUStudent <wustudent@webster.edu>
2026-09-04 16:29:27 -0500

    zombie guts

---
+++ FPSUnity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damageToGive;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Zombie>())
+        {
+            //Collision with zombie
+            collision.gameObject.GetComponent<Zombie>().TakeDamage(damageToGive);
+
+        }
+    }
+
+
+}
--- FPSUnity/Assets/Scripts/canon.cs
+++ FPSUnity/Assets/Scripts/canon.cs
@@ -6,7 +6,7 @@ public class canon : MonoBehaviour
     public float shootVelocity;
     public GameObject projectilePrefab;
     public AudioSource shootSound;
-
+    public Transform projectileSpawnPoint;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,11 +18,26 @@ public class canon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     void Shoot()
     {
+        print("Shoot");
 
+        // Step 1: instantiate /Spawn projectile prefab
+        GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+
+        // Step 2: add forward force to it
+        newProjectile.GetComponent<Rigidbody>().AddForce(projectileSpawnPoint.forward * shootVelocity);
+
+        //Step 3: add sound effect
+        shootSound.Play();
+
+        //Step 4: despawn projectiles
+        Destroy(newProjectile, 5);
     }
 }
