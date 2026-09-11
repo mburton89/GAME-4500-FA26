918541bdc7ce44d02a4f237b0dd18a28d2a942cd
ScarletStarborne <159509215+ScarletStarborne@users.noreply.github.com>
2026-09-11 16:41:28 -0500

    zomibe

--- FPSUnity/Assets/Scripts/Zombie.cs
+++ FPSUnity/Assets/Scripts/Zombie.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
-
+using UnityEngine.UI;
 public class Zombie : MonoBehaviour
 {
     public float moveSpeed;
@@ -12,6 +12,8 @@ public class Zombie : MonoBehaviour
     public GameObject zombieGuts;
 
     public AudioSource takeDamageSound;
+
+    public Image healthBarFill;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,10 +43,14 @@ public class Zombie : MonoBehaviour
         currentHealth -= damageToTake;
         takeDamageSound.Play();
 
+        healthBarFill.fillAmount = currentHealth / maxHealth;
+
         if(currentHealth <= 0)
         {
             Instantiate(zombieGuts, transform.position, transform.rotation, null);
+            ZombieSpawner.Instance.CountCurrentZombies();
             Destroy(gameObject);
+
         }
     }
 }
