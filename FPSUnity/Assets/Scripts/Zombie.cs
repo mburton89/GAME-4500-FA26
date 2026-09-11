c41c033adbb6fe754a898d0c62be96b50dc673dc
Piptart44 <nr.cruzcht@gmail.com>
2026-09-11 16:40:04 -0500

    Zombie Spawners

--- FPSUnity/Assets/Scripts/Zombie.cs
+++ FPSUnity/Assets/Scripts/Zombie.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Zombie : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class Zombie : MonoBehaviour
     public GameObject zombieGuts;
 
     public AudioSource takeDamageSound;
+
+    public Image healthBarFill;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -44,9 +47,12 @@ public class Zombie : MonoBehaviour
         currentHealth -= damageToTake;
         takeDamageSound.Play();
 
+        healthBarFill.fillAmount = currentHealth / maxHealth;
+
         if (currentHealth <= 0)
         {
             Instantiate(zombieGuts, transform.position, transform.rotation, null);
+            ZombieSpawner.Instance.CountCurrentZombies();
             Destroy(gameObject);
         }
     }
---
+++ FPSUnity/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    public static ZombieSpawner Instance;
+
+    int wave;
+    public GameObject zombiePrefab;
+    public List<Transform> zombieSpawnPoints;
+
+    public TextMeshProUGUI waveText;
+
+    private void Awake()
+    {
+        Instance = this;
+        wave = 1;
+    }
+
+    public void SpawnWaveOfZombies()
+    {
+        print("SpawWaveOfZombies");
+
+        wave++;
+        waveText.SetText("wave: " + wave);
+
+        for (int i = 0; i < wave; i++)
+        {
+            int rand = Random.Range(0, zombieSpawnPoints.Count);
+            Instantiate(zombiePrefab, zombieSpawnPoints[rand].position, transform.rotation, transform); 
+        }
+        
+    }
+
+    public void CountCurrentZombies()
+    {
+        Zombie[] allZombiesInScene = FindObjectsByType<Zombie>(FindObjectsSortMode.None);
+        
+        if(allZombiesInScene.Length ==1)
+        {
+            SpawnWaveOfZombies();
+        }
+    }
+
+}
